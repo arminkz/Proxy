a1d9ab582c38e00122880f2c0b5ac7554ef2fc66
Armin Kazemi <arminkz@live.com>
2018-06-01 18:15:00 +0430

    DNS Record Database

--- dns/dnsproxy.rb
+++ dns/dnsproxy.rb
@@ -47,7 +47,7 @@ class ResourceRecord < DnsRecord
 end
 
 class DnsPacket < BinData::Record
-	dns_header :header
+  dns_header :header
   array :questions,    type: :question,        initial_length: lambda { header.q_count }
   array :answers,      type: :resource_record, initial_length: lambda { header.a_count }
   array :authorities,  type: :resource_record, initial_length: lambda { header.ns_count }
@@ -57,10 +57,25 @@ end
 
 class DNSServer
   attr_reader :port, :ttl
-  attr_accessor :records
 
-  def initialize(port: 5300 , ttl: 60 , records: {})
-    @port, @records, @ttl = port, records, ttl
+  def initialize(port: 5300 , ttl: 60)
+    @port, @ttl = port, ttl
+
+    #DNS Database
+    @records = {
+      "4kp.ir." => "185.55.277.57",
+      "google.com." => "172.217.18.174"
+    }
+  end
+
+  def get_query_url(rcvd)
+    url = ""
+    for n in rcvd.questions[0].rnameseq.names
+      if (n.nelen > 0)
+        url += n.name + '.'
+      end
+    end
+    return url
   end
 
   def create_response(rcvd,ip)
@@ -91,6 +106,24 @@ class DNSServer
     return resp
   end
 
+  def create_empty_response(rcvd)
+    resp = DnsPacket.new
+    resp.header = DnsHeader.new
+    resp.header.transaction_id = rcvd.header.transaction_id
+    resp.header.flag_QR = 1
+    resp.header.opcode = 0
+    resp.header.flag_AA = 0
+    resp.header.flag_RD = 0
+    resp.header.flag_RA = 0
+    resp.header.rcode = 0
+    resp.header.q_count = 1
+    resp.header.a_count = 0
+    resp.header.ns_count = 0
+    resp.header.ar_count = 0
+    resp.questions = rcvd.questions
+    return resp
+  end
+
   def run
     puts "Starting DNS Proxy Server on port #{@port} ..."
     @socket = UDPSocket.new
@@ -102,15 +135,24 @@ class DNSServer
       Socket.udp_server_loop(@port) do |data, src|
       	r = DnsPacket.new
         r.read(data)
+        #puts r
+
+        url = get_query_url(r)
+        puts url
 
         #Build Up Response
-        resp = create_response(r,"10.0.2.15")
+        if(@records.has_key?(url))
+          resp = create_response(r,@records[url])
+        else
+          resp = create_empty_response(r)
+        end
 
         src.reply resp.to_binary_s
       end
 
     rescue Interrupt
       puts "Got Interrupt !"
+
     ensure
       if @socket
         @socket.close
